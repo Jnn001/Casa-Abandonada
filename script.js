06800a9a0e37bad20022e089e01f13bb749ccc91
Jnn001 <0000110567955xsp@al.educacao.sp.gov.br>
2024-08-29 23:03:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const avanca = document.querySelectorAll('.b-avanca');
+const avanca = document.querySelectorAll('.b-avancar');
 
 avanca.forEach = (button => {
   button.addEventListener('click', function(){
     const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-' + this.getAtribute('data-avancar');
+    const proximoPasso = 'passo-' + this.getAttribute('data-avancar');
 
     atual.classList.remove('ativo');
     document.getElementById('proximoPasso').classList.add('ativo');
